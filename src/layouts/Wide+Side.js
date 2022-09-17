ee2267c01c4e9ce61fb4d4a0da95259787f3f3ac
Optimizitor <sapomaro@gmail.com>
2022-09-17 19:57:46 +0300

    hotfix for parcel

--- src/layouts/Wide+Side.js
+++ src/layouts/Wide+Side.js
@@ -1,5 +1,6 @@
-import('/src/components/Head.js');
 import ProtoPages from '/src/templator/ProtoPages.js';
+
+import '/src/components/Head.js';
 
 import '/src/layouts/common.scss';
 import '/src/layouts/Wide.scss';
--- src/layouts/Wide.js
+++ src/layouts/Wide.js
@@ -1,6 +1,7 @@
 import ProtoPages from '/src/templator/ProtoPages.js';
 
-import('/src/components/Head.js');
+import '/src/components/Head.js';
+
 import '/src/layouts/common.scss';
 import '/src/layouts/Wide.scss';
 
