1aa44c1de6c12861829cea8c94b9a9b33da067e2
Optimizitor <sapomaro@gmail.com>
2022-10-26 08:17:19 +0300

    chats api clean up

--- src/api/chats.ts
+++ src/api/chats.ts
@@ -1,8 +1,5 @@
 import {ajax} from '../core/Ajax';
-import {chatsWebSocketUrl} from './base';
-import {EventBus} from '../core/EventBus';
-import {JSONWrapper} from '../core/Utils';
-import type {RequestT, MessageT} from '../constants/types';
+import type {RequestT} from '../constants/types';
 
 export const chatsAPI = {
   getChats: () =>
@@ -19,44 +16,4 @@ export const chatsAPI = {
     ajax.delete('/chats/users', data),
   getChatToken: (chatId: number) =>
     ajax.post(`/chats/token/${chatId}`),
-};
-
-export const chatsSocketAPI: {
-  socket: WebSocket | null;
-  init: (data: RequestT['SocketInit']) => void;
-  send: (data: MessageT) => void;
-} = {
-  socket: null,
-  init: ({userId, chatId, token}) => {
-    if (chatsSocketAPI.socket) {
-      chatsSocketAPI.socket.close();
-    }
-    chatsSocketAPI.socket =
-      new WebSocket(`${chatsWebSocketUrl}/${userId}/${chatId}/${token}`);
-    const socket = chatsSocketAPI.socket;
-
-    EventBus.emit('webSocketInit');
-
-    socket.addEventListener('open', () => {
-      EventBus.emit('webSocketOpen');
-    });
-
-    socket.addEventListener('close', (event: CloseEvent) => {
-      EventBus.emit('webSocketClose', event);
-    });
-
-    socket.addEventListener('message', (event: MessageEvent) => {
-      EventBus.emit('webSocketMessage', event.data);
-    });
-
-    socket.addEventListener('error', (event: ErrorEvent) => {
-      EventBus.emit('webSocketError', event);
-    });
-  },
-  send: (data) => {
-    const socket = chatsSocketAPI.socket;
-    if (socket) {
-      socket.send(JSONWrapper.stringify(data));
-    }
-  },
 };
