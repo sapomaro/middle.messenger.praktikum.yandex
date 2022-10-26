67a08eebaf1b262b82a719b29441faf7360f6fcd
Optimizitor <sapomaro@gmail.com>
2022-10-26 08:17:29 +0300

    Update HTTPTransport.ts

--- src/core/HTTPTransport.ts
+++ src/core/HTTPTransport.ts
@@ -8,141 +8,157 @@ enum METHOD {
   DELETE = 'DELETE'
 }
 
-export type AJAXOptionsT = {
+export type OptionsT = {
   url: string;
   method?: METHOD;
   tries?: number;
   timeout?: number;
   headers?: Record<string, string>;
   data?: unknown;
-}
-
-export class HTTPTransport {
-
-}
-
-function AJAXRequest(options: AJAXOptionsT) {
-  const {
-    url,
-    method = METHOD.GET,
-    data = '',
-    tries = 0,
-    headers = {},
-  } = options;
-  const dataType = (typeof data === 'string') ? 'string' :
-    (typeof data === 'object' && data instanceof FormData) ? 'formdata' :
-    (isArrayOrObject(data)) ? 'json' : 'unknown';
-  const urlParams = (method === METHOD.GET && dataType === 'json') ?
-    getUrlParams(data as Record<string, unknown>) : '';
-  setRequestHeaders(headers, dataType);
-
-  return new Promise((resolve, reject) =>
-    (function request(options) {
-      const handleError = (error: unknown): void => {
-        if (tries) {
-          request(url, options);
-        } else {
-          reject(error);
-        }
-      };
-      const xhr = new XMLHttpRequest();
-      xhr.open(method, ajax.baseUrl + url + urlParams, true);
-      xhr.withCredentials = true;
-      xhr.timeout = (options.timeout ? options.timeout : 3000);
-      for (const [key, value] of Object.entries(headers)) {
-        xhr.setRequestHeader(key, value);
-      }
-      if (method === METHOD.GET || !data) {
-        xhr.send();
-      } else if (dataType === 'json') {
-        try {
-          xhr.send(JSON.stringify(data as PlainObject));
-        } catch (error) {
-          handleError(error);
-        }
-      } else {
-        xhr.send(data as string | Blob);
-      }
-      xhr.onreadystatechange = (): void => {
-        if (xhr.readyState === 4) {
-          const responseText = xhr.responseText.replace(/^\"|\"$/g, '');
-          const responseHeaders = getResponseHeaders(xhr);
-          let responseJSON: Record<string, unknown> = {};
-          if (responseHeaders['content-type'] &&
-              responseHeaders['content-type']
-                  .indexOf('application/json') !== -1) {
-            try {
-              responseJSON = JSON.parse(responseText);
-            } catch (error) {
-              reject(error);
-            }
-          }
-          const response = {
-            responseText,
-            responseJSON,
-            responseHeaders,
-            status: xhr.status,
-          };
-          if (xhr.status >= 200 && xhr.status <= 399) {
-            resolve(response);
-          } else if (xhr.status >= 400) {
-            reject(response);
-          }
-        }
-      };
-      xhr.onabort = handleError;
-      xhr.onerror = handleError;
-      xhr.ontimeout = handleError;
-    })(options),
-  );
-
-
-  return xhr;
 };
 
-function setRequestHeaders(headers, dataType) {
-  if (!headers['accept']) {
-    headers['accept'] = 'application/json';
+export type ResponseT = {
+  responseText: string;
+  responseJSON: PlainObject
+  responseHeaders: PlainObject;
+  status: number;
+} | Error;
+
+export class HTTPTransport {
+  public baseUrl = '';
+  get(url: string, data?: unknown): Promise<ResponseT> {
+    return this.request({url, method: METHOD.GET, data});
   }
-  if (!headers['Content-Type']) {
-    if (dataType === 'json') {
-      headers['Content-Type'] = 'application/json; charset = UTF-8';
-    } else if (dataType === 'formdata') {
-      headers['Content-Type'] = 'multipart/form-data';
+  post(url: string, data?: unknown): Promise<ResponseT> {
+    return this.request({url, method: METHOD.POST, data});
+  }
+  put(url: string, data?: unknown): Promise<ResponseT> {
+    return this.request({url, method: METHOD.PUT, data});
+  }
+  delete(url: string, data?: unknown): Promise<ResponseT> {
+    return this.request({url, method: METHOD.DELETE, data});
+  }
+  request(options: OptionsT): Promise<ResponseT> {
+    let {
+      url,
+      method = METHOD.GET,
+      data = '',
+      tries = 1,
+      timeout = 3000,
+      headers = {},
+    } = options;
+    const dataType = (typeof data === 'string') ? 'string' :
+      (typeof data === 'object' && data instanceof FormData) ? 'formdata' :
+      (isArrayOrObject(data)) ? 'json' : 'unknown';
+    const urlParams = (method === METHOD.GET && dataType === 'json') ?
+      this.getUrlParams(data as Record<string, unknown>) : '';
+    this.setRequestHeaders(headers, dataType);
+
+    return new Promise((resolve, reject) =>
+      (function request() {
+        const xhr = new XMLHttpRequest();
+        xhr.open(method, this.baseUrl + url + urlParams, true);
+        xhr.withCredentials = true;
+        xhr.timeout = timeout;
+        for (const [key, value] of Object.entries(headers)) {
+          xhr.setRequestHeader(key, value);
+        }
+        if (method === METHOD.GET || !data) {
+          xhr.send();
+        } else if (dataType === 'json') {
+          try {
+            xhr.send(JSON.stringify(data as PlainObject));
+          } catch (error) {
+            reject(error);
+          }
+        } else {
+          xhr.send(data as string | Blob);
+        }
+        xhr.onreadystatechange = (): void => {
+          if (xhr.readyState === 4) {
+            const responseText = xhr.responseText.replace(/^\"|\"$/g, '');
+            const responseHeaders = this.getResponseHeaders(xhr);
+            let responseJSON: Record<string, unknown> = {};
+            if (responseHeaders['content-type'] &&
+                responseHeaders['content-type']
+                    .indexOf('application/json') !== -1) {
+              try {
+                responseJSON = JSON.parse(responseText);
+              } catch (error) {
+                reject(error);
+              }
+            }
+            const response: ResponseT = {
+              responseText,
+              responseJSON,
+              responseHeaders,
+              status: xhr.status,
+            };
+            if (xhr.status >= 200 && xhr.status <= 399) {
+              resolve(response);
+            } else if (xhr.status >= 400) {
+              reject(response);
+            }
+          }
+        };
+        const handleError = (error: ProgressEvent): void => {
+          if (--tries > 0) {
+            request();
+          } else {
+            reject(error);
+          }
+        };
+        xhr.onabort = handleError;
+        xhr.onerror = handleError;
+        xhr.ontimeout = handleError;
+      })(),
+    );
+  }
+
+  setRequestHeaders(headers: PlainObject, dataType: string) {
+    if (!headers['accept']) {
+      headers['accept'] = 'application/json';
+    }
+    if (!headers['Content-Type']) {
+      if (dataType === 'json') {
+        headers['Content-Type'] = 'application/json; charset = UTF-8';
+      } else if (dataType === 'formdata') {
+        headers['Content-Type'] = 'multipart/form-data';
+      }
     }
   }
-}
 
-function getResponseHeaders(xhr: XMLHttpRequest) {
-  const headers: Record<string, string> = {};
-  const headersString = xhr.getAllResponseHeaders();
-  const headersList = headersString.trim().split('\r\n');
-  for (const header of headersList) {
-    const [key, value] = header.split(': ');
-    headers[key.toLowerCase()] = value.toLowerCase();
-  }
-  return headers;
-}
-
-function getKey(key: string, parentKey?: string) {
-  return parentKey ? `${parentKey}[${key}]` : key;
-}
-
-function getParams(data: PlainObject | [], parentKey?: string) {
-  const result: [string, string][] = [];
-  for (const [key, value] of Object.entries(data)) {
-    if (isArrayOrObject(value)) {
-      result.push(...getParams(value, getKey(key, parentKey)));
-    } else {
-      result.push([getKey(key, parentKey), encodeURIComponent(String(value))]);
+  getResponseHeaders(xhr: XMLHttpRequest) {
+    const headers: Record<string, string> = {};
+    const headersString = xhr.getAllResponseHeaders();
+    const headersList = headersString.trim().split('\r\n');
+    for (const header of headersList) {
+      const [key, value] = header.split(': ');
+      headers[key.toLowerCase()] = value.toLowerCase();
     }
+    return headers;
   }
-  return result;
-}
 
-function getUrlParams(data: PlainObject) {
-  if (!isPlainObject(data)) {
-    throw new Error('input must be an object');
+  getKey(key: string, parentKey?: string) {
+    return parentKey ? `${parentKey}[${key}]` : key;
   }
-  return getParams(data).map((arr) => arr.join('=')).join('&');
+
+  getParams(data: PlainObject | [], parentKey?: string) {
+    const result: [string, string][] = [];
+    for (const [key, value] of Object.entries(data)) {
+      if (isArrayOrObject(value)) {
+        result.push(...this.getParams(value, this.getKey(key, parentKey)));
+      } else {
+        result.push([this.getKey(key, parentKey), encodeURIComponent(String(value))]);
+      }
+    }
+    return result;
+  }
+
+  getUrlParams(data: PlainObject) {
+    if (!isPlainObject(data)) {
+      throw new Error('input must be an object');
+    }
+    return this.getParams(data).map((arr) => arr.join('=')).join('&');
+  }
 }
